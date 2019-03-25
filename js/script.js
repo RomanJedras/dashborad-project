0a927389b949414967d4cdc71a3650b3ec65c015
Roman Jędras <romanjedras@o2.pl>
2019-03-25 18:19:14 +0100

    Add modal window

--- js/script.js
+++ js/script.js
@@ -20,6 +20,7 @@ navIcon.addEventListener('click',function () {
     linkItem.classList.toggle('link-nav');
 })
 
+
 var ctx = document.getElementById('myChart').getContext('2d');
 var chart = new Chart(ctx, {
     // 1
@@ -61,6 +62,46 @@ var chart = new Chart(ctx, {
             }]
     },
 });
+
+
+function closeModal() {
+    document.getElementById('overlay').classList.remove('showModal')
+}
+
+document.querySelectorAll('#overlay .js--close-modal').forEach(function(btn) {
+    btn.addEventListener('click', function(e) {
+        e.preventDefault()
+        closeModal()
+    })
+})
+
+document.querySelector('#overlay').addEventListener('click', function(e) {
+    if(e.target === this) {
+        closeModal()
+    }
+})
+
+document.addEventListener('keyup', function(e) {
+    if(e.keyCode === 27) {
+        closeModal()
+    }
+})
+
+function openModal(modal) {
+    document.querySelectorAll('#overlay > *').forEach(function(modal) {
+        modal.classList.remove('showModal')
+    })
+    document.querySelector('#overlay').classList.add('showModal')
+    document.querySelector('.modal').classList.add('showModal')
+}
+
+
+let button = document.getElementById('modal');
+
+  button.addEventListener('click',openModal);
+
+
+
 
 
 const ulList = document.getElementById("nav"),
